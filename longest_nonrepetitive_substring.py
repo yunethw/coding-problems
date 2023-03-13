1474e1abe9ec88387263a7bc4650c5c075aaa2b5
Yuneth Wijenayake <yunethcwij@gmail.com>
2023-03-14 01:11:28 +0530

    converted to function

--- longest_nonrepetitive_substring.py
+++ longest_nonrepetitive_substring.py
@@ -2,19 +2,24 @@
 
 input_string = input()
 
-substring = ""  # store the longest substring
-current_string = ""  # store the current substring in the loop
 
-for char in input_string:
-    if char not in current_string:
-        current_string += char
+def longest_nonrepetitive_substring(string):
+    substring = ""  # store the longest substring
+    current_string = ""  # store the current substring in the loop
+
+    for char in string:
+        if char not in current_string:
+            current_string += char
+        else:
+            if len(current_string) > len(substring):
+                substring = current_string
+            current_string = char
     else:
-        if len(current_string) > len(substring):
+        if current_string == string or len(current_string) > len(substring):
             substring = current_string
-        current_string = char
-else:
-    if current_string == input_string or len(current_string) > len(substring):
-        substring = current_string
 
-# prints longest substring and length (incase of two longest substrings, it will print first)
-print(substring, len(substring))
+    return substring
+
+
+result = longest_nonrepetitive_substring(input_string)
+print(result, len(result))
